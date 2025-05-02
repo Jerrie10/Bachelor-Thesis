9e9947dbd175106e6c759e0ab5a5895db4544010
Jeroen <jeroen.ockers@gmail.com>
2025-05-02 14:25:35 +0200

    Health data toegevoegd

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,33 @@
 import numpy as np
 import geopy.distance as gpd
+import pandas as pd
+import requests
+import json
+
+
+def health_coords(inputfile, outputfile):
+    df = pd.read_csv(inputfile, sep=';')
+    pd.set_option('display.max_rows', None)
+
+    for i, row in df.iterrows():
+        apiAddress = str(df.at[i,'Adres'])
+        
+        parameters = {
+            "key": "huVtHgCHgRNOzWrZAE9bl7bN9Eak6Utz",
+            "location": apiAddress
+        }
+
+        response = requests.get("http://www.mapquestapi.com/geocoding/v1/address", params=parameters)
+        
+        data = response.text
+        dataJ = json.loads(data)['results']
+        lat = (dataJ[0]['locations'][0]['latLng']['lat'])
+        lng = (dataJ[0]['locations'][0]['latLng']['lng'])
+        
+        df.at[i,'lat'] = lat
+        df.at[i,'lng'] = lng
+        
+    df.to_csv(outputfile)
 
 def distance(x, y, taxicab=False):
     """Calculates geodesic distance (km) between two tuples of coordinates.
@@ -17,3 +45,7 @@ def distance(x, y, taxicab=False):
                                y).km)
 
 
+def main():
+    health_coords("healthlocations.csv", "Data/healthdata.csv")
+
+main()
