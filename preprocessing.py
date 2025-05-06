847bc0eabd09abd8f4c5383eb869bb3c24c68010
Jeroen <jeroen.ockers@gmail.com>
2025-05-06 17:19:02 +0200

    Finished add_walking

--- preprocessing.py
+++ preprocessing.py
@@ -238,17 +238,116 @@ def transit_processing(stop_file, route_file, stop_time_file,
 
         print("Done processing route "+str(r))
 
+# -------------------------------------------------------------------------------------------------
+def add_walking(stop_file, arc_file, cutoff = 0.25):
+    """Generates walking arcs between stops.
 
+    Requires the name of the arc file and the stop file.
+
+    Accepts an optional keyword argument to specify the (taxicab) distance
+    cutoff (km), within which walking arcs will be generated.
+
+    Clusters within the cutoff distance of each other will receive a pair of
+    walking arcs between them, with a travel time calculated based on the
+    distance and the walking speed defined above.
+
+    #In order to reduce the number of arcs in densely-packed clusters of stops,
+    we prevent arcs from being generated between pairs of stops if the
+    quadrangle defined by them contains another stop.
+    """
+    ids = []
+    coords = []
+    df = pd.read_csv(stop_file, sep=';')
+    for i, row in df.iterrows():
+        ids.append(row['ID'])
+        coords.append((row['lat'].replace(',', '.'), row['lng'].replace(',', '.')))
+
+    # Go through each unique coordinate pair and generate a dictionary of pairs
+    # within the cutoff distance of each other
+    count = 0
+    pairs = {}
+    for i in range(len(coords)):
+        print("Iteration "+str(i+1)+" / "+str(len(coords)))
+
+        for j in range(i):
+            # Calculate pairwise distance
+            dist = distance(coords[i], coords[j], taxicab=True)
+            if dist <= cutoff:
+                keep = True # whether to keep the current pair
+
+                # Define corners of quadrangle as most extreme lat/lon in pair
+                lat_min = min(coords[i][0], coords[j][0])
+                lat_max = max(coords[i][0], coords[j][0])
+                lon_min = min(coords[i][1], coords[j][1])
+                lon_max = max(coords[i][1], coords[j][1])
+
+                # Scan entire stop list for stops within the quadrangle
+                for k in range(len(coords)):
+                    if (k != i) and (k != j):
+                        if ((lat_min <= coords[k][0] <= lat_max) and
+                            (lon_min <= coords[k][1] <= lon_max)):
+                            # Stop found in quadrangle, making pair invalid
+                            keep = False
+                            break
+
+                # If no stops were found in the quadrangle, then we add the
+                # pair along with their walking time to the dictionary
+                if keep == True:
+                    count += 1
+                    pairs[(ids[i], ids[j])] = dist * km_walk_time
+
+    # Use the final pairs dictionary to generate the new arcs and write them to
+    # the arc file
+    with open(arc_file, 'r+') as f:
+        # Count the number of arcs
+        arcnum = -np.inf
+        f.readline()
+        for line in f:
+            li = f.readline().split()
+            if len(li) > 0:
+                # Skip the blank line at the end
+                arcnum = max(arcnum, int(li[0]))
+        arcnum += 1
+
+        for p in pairs:
+            # ID, Type, Line, Tail, Head, Time
+            print(str(arcnum)+"\t"+str(aid_walk)+"\t-1\t"+str(p[0])+"\t"+
+                  str(p[1])+"\t"+str(pairs[p]), file=f)
+            arcnum += 1
+            print(str(arcnum)+"\t"+str(aid_walk)+"\t-1\t"+str(p[1])+"\t"+
+                  str(p[0])+"\t"+str(pairs[p]), file=f)
+            arcnum += 1
+
+    print("Done. Added a total of "+str(count)+" pairs of walking arcs.")
+
+
+
+    pass
+
+
+#TODO ---------------------------------------------------------------------------------------------
+#def cluster_boarding   (need user data)
+#def gamma              (need user data)
+#def all_times          (need user data)
+#def od_matrix          (need user data)
+
+# -------------------------------------------------------------------------------------------------
+#def network_assemble()
+
+#TODO ---------------------------------------------------------------------------------------------
+#def transit_finalization
+#def misc_files
+# -------------------------------------------------------------------------------------------------
 
 def main():
     #(un)comment lines based on what needs to be processed
-
+    # TODO rerun everything to avoid duplicate arcs/nodes
     #address_to_coords(facility_raw, facility_in)
     #facility_processing(facility_in, facility_out)
     #stop_processing(stop_data, time_data, route_times)
     
-    transit_processing(stop_data, route_data, route_times, line_nodes, line_arcs)
-    
+    #transit_processing(stop_data, route_data, route_times, line_nodes, line_arcs)
+    #add_walking(stop_data, line_arcs)
 
     pass
 main()
