bd23ec69a7e1f3edbbda09d812823a129a095706
Jeroen <jeroen.ockers@gmail.com>
2025-10-30 17:04:02 +0100

    Experimenting 3 times

---
+++ Results/results.py
@@ -0,0 +1,19 @@
+''' Program that uses results of Transit Solver and maken figures to display those results
+
+'''
+
+import numpy as np
+import matplotlib as plt
+
+
+
+
+
+
+def main():
+    
+
+
+
+    pass
+main()
